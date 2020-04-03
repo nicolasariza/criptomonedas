3f318b88829b256e392ccba17651620d525a859e
nicolasariza <nicolas_ariza@outlook.com>
2020-04-03 18:00:46 -0500

    Updates

---
+++ precache-manifest.e20dc626e2a3ddfbc837574d22f1ec43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8def6229145b67b9c4d4a8faf2bbe637",
+    "url": "/criptomonedas/index.html"
+  },
+  {
+    "revision": "741e2e908f2079c756e0",
+    "url": "/criptomonedas/static/css/main.730bf9d1.chunk.css"
+  },
+  {
+    "revision": "a641313abe21a618eec3",
+    "url": "/criptomonedas/static/js/2.2f355375.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/criptomonedas/static/js/2.2f355375.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "741e2e908f2079c756e0",
+    "url": "/criptomonedas/static/js/main.fca7e70b.chunk.js"
+  },
+  {
+    "revision": "31efd766ebdc68dea720",
+    "url": "/criptomonedas/static/js/runtime-main.f68809a4.js"
+  },
+  {
+    "revision": "96e45d694833f161c9dc02ab9dde4a6b",
+    "url": "/criptomonedas/static/media/cryptomonedas.96e45d69.png"
+  }
+]);
